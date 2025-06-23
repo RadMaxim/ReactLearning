3eda4043ae48b5c00f0fac8123c5ba0bac580cb5
m.radchuk <m.radchuk@farnell.by>
2025-06-23 13:22:27 +0300

    1718

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,8 @@ import { styleBox, styleList } from "./style";
 function App() {
     console.log("15")
     console.log("16")
+    console.log("17")
+    console.log("18")
   return (
     <SearchProvider>
       <Box style={styleBox}>
