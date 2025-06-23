c89c8fd16692e356f5f666a9fec946c959fa12c4
m.radchuk <m.radchuk@farnell.by>
2025-06-23 09:28:35 +0300

    refactor: создал новый файл

--- src/App.tsx
+++ src/App.tsx
@@ -4,20 +4,11 @@ import {Box} from "@mui/material";
 import UniversityList from "./Page/UniversityList/UniversityList";
 import SearchInput from "./component/SearchInput/SearchInput";
 import {SearchProvider} from "./Context/SearchContext";
+import {styleBox, styleList} from "./style";
 
 function App() {
-  const styleBox = {
-    backgroundColor: "var(--blue-back)",
-    color: "var(--blue-text)",
-    width: "100%",
 
-  }
-  const styleList = {
-    overflow: "auto",
-    height: "73vh",
-    width: "100%",
 
-  }
   return (
       <SearchProvider>
     <Box style={styleBox}>
---
+++ src/style.ts
@@ -0,0 +1,12 @@
+export const styleList = {
+    overflow: "auto",
+    height: "73vh",
+    width: "100%",
+
+}
+export const styleBox = {
+    backgroundColor: "var(--blue-back)",
+    color: "var(--blue-text)",
+    width: "100%",
+
+}
