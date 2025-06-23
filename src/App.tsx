755f6cc13d4542a60753602dc704875ba49de752
m.radchuk <m.radchuk@farnell.by>
2025-06-23 13:36:54 +0300

    5678

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,10 @@ function App() {
     console.log("2")
     console.log("3")
     console.log("4")
+    console.log("5")
+    console.log("6")
+    console.log("7")
+    console.log("8")
   return (
     <SearchProvider>
       <Box style={styleBox}>
