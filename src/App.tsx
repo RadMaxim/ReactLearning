6d94b6fb32bcdca5031e6963b8438fc73c56b6ac
m.radchuk <m.radchuk@farnell.by>
2025-06-23 13:51:18 +0300

    feat: изменил App

--- src/App.tsx
+++ src/App.tsx
@@ -15,6 +15,22 @@ function App() {
     console.log("6")
     console.log("7")
     console.log("8")
+    console.log("1")
+    console.log("2")
+    console.log("3")
+    console.log("4")
+    console.log("5")
+    console.log("6")
+    console.log("7")
+    console.log("8")
+    console.log("1")
+    console.log("2")
+    console.log("3")
+    console.log("4")
+    console.log("5")
+    console.log("6")
+    console.log("7")
+    console.log("8")
   return (
     <SearchProvider>
       <Box style={styleBox}>
